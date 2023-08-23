e9822d00b1422f97ac7cf846ec434672825e744d
Harman <harmanmalova@gmail.com>
2023-08-23 12:24:10 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,125 @@
-import Image from 'next/image'
+'use client'
+import { useEffect, useReducer, useRef,  } from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+export const metadata = {
+  title: 'Harmans home',
+  description: 'Harmans brand',
 }
+//   const[count, setCount] = useState(0)
+
+// const handleIncrease = () => {
+//   setCount((prev) => prev + 1)
+// }
+
+// const handleReset = () => {
+//   setCount((prev) => prev = 0);
+// }
+
+// const handleDecrease = () => {
+//   setCount((prev) => prev - 1)
+// }
+
+// const [time, setTime] = useState(0)
+
+// useEffect(()=>{
+//   setInterval(()=>{
+//     setTime((prev) => prev + 1)
+//   }, 1000)
+// }, [])
+export default function Home() {
+  const initialState = {
+    name: '',
+    email: '',
+  };
+
+  const reducer = (state, action) => {
+    return {
+      ...state,
+      ...action,
+    };
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+
+  const handleNameChange = (event) => {
+    dispatch({ name: event.target.value });
+  };
+
+  const handleEmailChange = (event) => {
+    dispatch({ email: event.target.value });
+  };
+
+  useEffect(()=>{
+    nameInputRef.current.focus()
+  
+  }, [])
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Name:', nameInputRef.current.value);
+    console.log('Email:', emailInputRef.current.value);
+    dispatch({ name: '', email: '' });
+    nameInputRef.current.focus();
+    
+    
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-slate-500 rounded-lg p-8 shadow-md">
+        <h1 className="text-2xl font-semibold mb-4">User Information</h1>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-black mb-1">Name</label>
+          <input
+            required
+            ref={nameInputRef}
+            type="text"
+            className="w-full p-2 border border-gray-300  text-gray-700 rounded focus:outline-none focus:border-blue-500"
+            value={state.name}
+            onChange={handleNameChange}
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-black mb-1">Email</label>
+          <input
+            required
+            ref={emailInputRef}
+            type="text"
+            className="w-full p-2 border border-gray-300 text-gray-700 rounded focus:outline-none focus:border-blue-500"
+            value={state.email}
+            onChange={handleEmailChange}
+            placeholder="Enter your email"
+          />
+        </div>
+        <div>
+          <p className="text-md text-gray-600 font-bold">Name: {state.name}</p>
+          <p className="text-md text-gray-600 font-bold">Email: {state.email}</p>
+        </div>
+          <button type="submit" className="p-3 rounded-md bg-blue-900 text-white w-full" onClick={handleFormSubmit}>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
+    
+
+    {/* <p className="text-white font-bold">Counter: {count}</p> */}
+
+    {/* <div className="font-xl font-bold bg-slate-700 text-white"> Timer: {time} </div> */}
+     
+
+      {/* <div className="flex mr-3">
+      <button onClick={handleIncrease} className="px-4 py-3 rounded-md bg-blue-500 text-white w-30">increase</button>
+      <button onClick={handleReset} className="px-4 py-3 rounded-md bg-blue-500 w-30 text-white">Reset</button>
+     <button onClick={handleDecrease}className="px-4 rounded-md w-30 py-3 bg-blue-500 text-white">Decrease</button>
+      </div> */}
---
+++ constants/index.js
@@ -0,0 +1,27 @@
+export const navlinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '/'
+  },
+  {
+    id: 2,
+    name: 'Contacts',
+    href: '/contant'
+  },
+  {
+    id: 3,
+    name: 'Services',
+    href: '/services'
+  },
+  {
+    id: 4,
+    name: 'About',
+    href: '/about'
+  },
+  {
+    id: 5,
+    name: 'Career',
+    href: '/recruitment'
+  },
+]
